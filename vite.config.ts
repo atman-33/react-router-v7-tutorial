d325db7be3de7e08571f988050c09b32616280d2
atman-33 <gpbjk0304@gmail.com>
2025-02-11 15:03:15 +0900

    feat: vite.config.tsで環境変数を扱えるように変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    server: {
+      port: Number.parseInt(env.PORT || '5173'),
+    },
+  };
 });
