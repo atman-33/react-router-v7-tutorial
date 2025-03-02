7f7d96c7ea96bd7b27aa32b13e85b0a25868cc7a
atman-33 <gpbjk0304@gmail.com>
2025-03-02 12:03:31 +0900

    chore: storybookをv8.6にアップグレード

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,8 +3,13 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../app/**/*.mdx', '../app/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        docs: false,
+      },
+    },
     '@storybook/addon-onboarding',
-    '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
   ],
